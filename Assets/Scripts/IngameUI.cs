62246d7effe91f45f1d9cae9a036d2b9a66af0f2
tjwngur1731 <1sjhjs1@gmail.com>
2021-08-10 18:39:13 +0900

    Title, Ingame UI

---
+++ Assets/Scripts/IngameUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngameUI : MonoBehaviour
+{
+    [SerializeField] private GameObject settingUI;
+    [SerializeField] private GameObject inventoryUI;
+
+    [SerializeField] private GameObject audioUI;
+    [SerializeField] private GameObject graphicUI;
+    [SerializeField] private GameObject saveUI;
+
+    public bool sfxOn;
+    public bool bgmOn;
+
+    public void SettingOnOff()
+    {
+        settingUI.SetActive(!settingUI.activeSelf);
+    }
+
+    public void SFXOnOff()
+    {
+        sfxOn = !sfxOn;
+    }
+
+    public void BGMOnOff()
+    {
+        bgmOn = !bgmOn;
+    }
+
+
+
+    public void InventoryOnOff()
+    {
+        inventoryUI.SetActive(!inventoryUI.activeSelf);
+    }
+}
---
+++ Assets/Scripts/TitleButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    [SerializeField] private GameObject settingUI; 
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Ingame");
+    }
+
+    public void GameExit()
+    {
+
+        Application.Quit();
+    }
+
+    public void Setting()
+    {
+        settingUI.SetActive(!settingUI.activeSelf);
+    }
+}
